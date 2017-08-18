3ddd376f9876d4fa800f811dfa98af32f48ef402
javimar <javimar@>
2017-08-18 19:54:38 +0200

    Add insterstitial logic

--- app/src/free/java/eu/javimar/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/eu/javimar/gradle/builditbigger/MainActivityFragment.java
@@ -1,36 +1,79 @@
 package eu.javimar.gradle.builditbigger;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment
+{
+    private InterstitialAd mInterstitialAd;
+    private AdRequest adRequest ;
+    private ProgressBar mProgressBar;
 
     public MainActivityFragment() {
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        AdView mAdView = rootView.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
+        adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
-        return root;
+
+        // set a listener to show the interstitial
+        Button button = rootView.findViewById(R.id.b_tell_joke);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }
+            }
+        });
+
+        // get the ProgressBar
+        mProgressBar = rootView.findViewById(R.id.progressBar);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        mInterstitialAd.loadAd(adRequest);
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() { // when interstitial closes, we want continue as before
+                // call AsynTask to retrieve jokes
+                new JokesAsyncTask(getActivity(), mProgressBar).execute();
+            }
+        });
     }
 }
--- javajokeslib/src/main/java/eu/javimar/gradle/jokes/Jokes.java
+++ javajokeslib/src/main/java/eu/javimar/gradle/jokes/Jokes.java
@@ -2,6 +2,7 @@ package eu.javimar.gradle.jokes;
 
 public class Jokes {
 
+    // Big library of jokes ;-)
     public String tellJokes() {
         return "This is a very funny joke";
     }
